6ca496c3020178905e61b157d388951265a4ad77
Da-nae <grosjean.danae@gmail.com>
2022-11-16 16:02:12 +0100

    functionnal but not pretty yet

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-// Calling the database : 
-const {Dataset} = require('data.js')
+#!/usr/bin/env node
+import axios from 'axios'
+import countryList from "country-list";
 
-const path = 'https://datahub.io/core/country-list/datapackage.json'
+const country = process.argv[2];
+const countryCode = countryList.getCode(country);
+const year = new Date().getFullYear();
 
-// We're using self-invoking function here as we want to use async-await syntax:
-;(async () => {
-    const dataset = await Dataset.load(path)
-    // get all tabular data(if exists any)
-    for (const id in dataset.resources) {
-        if (dataset.resources[id]._descriptor.format === "csv") {
-        const file = dataset.resources[id]
-        // Get a raw stream
-        const stream = await file.stream()
-        // print data
-        stream.pipe(process.stdout)
-        }
+async function getCountryCodeAsync(year, countryCode) {
+    return await axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`);
+}
+
+const response = getCountryCodeAsync(year, countryCode);
+
+response.then(datas => {
+    for (let d of datas.data) {
+        console.log(`${d.date}: ${d.name} - aka - ${d.localName}`);
     }
-})()
-
-// Calling the prompt to be able to read the input from the user :
-const prompt = require('prompt-sync')();
-
-// Creation of two parameters to host the country and code :
-const countryName = prompt('What country do you wish to know the holidays?');
-console.log(countryName);
-let countryCode;
+});
