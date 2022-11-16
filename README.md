5d36eed7f3f147a627d539540985cc9ce4d5628a
Da-nae <grosjean.danae@gmail.com>
2022-11-16 16:31:49 +0100

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # holidates-Jada-CLI
 Becode project to display the holiday dates from each country.
+
+To use it, write the command npx "@da-nae/holidates countryName" inside your terminal. You'll get the dates of all holidays from a country.
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import axios from 'axios'
+import chalk from 'chalk';
 import countryList from "country-list";
+import figlet from 'figlet';
 
 const country = process.argv[2];
 const countryCode = countryList.getCode(country);
@@ -12,8 +14,18 @@ async function getCountryCodeAsync(year, countryCode) {
 
 const response = getCountryCodeAsync(year, countryCode);
 
+figlet('Holidates !', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
 response.then(datas => {
+
     for (let d of datas.data) {
-        console.log(`${d.date}: ${d.name} - aka - ${d.localName}`);
+        console.log(chalk.cyan(`${d.date}:`), chalk.yellow(`${d.name}`), chalk.magenta(`- aka - ${d.localName}`));
     }
 });
